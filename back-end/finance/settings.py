38643addbe5d0d61c8ddda70e131259b18fdf20e
davex932 <tagnefonodavid@gmail.com>
2026-08-23 12:30:33 +0100

    debug: activer DEBUG=True par defaut pour diagnostiquer le 400

--- back-end/finance/settings.py
+++ back-end/finance/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 _allowed = os.getenv('ALLOWED_HOSTS', '')
 ALLOWED_HOSTS = _allowed.split(',') if _allowed else ['*']
