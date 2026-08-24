14ed23cea1c0dd462888e5a3fcded044d963fe26
davex932 <tagnefonodavid@gmail.com>
2026-08-24 10:22:19 +0100

    correction: forcer ALLOWED_HOSTS a accepter tous les hotes

--- back-end/finance/settings.py
+++ back-end/finance/settings.py
@@ -34,8 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-_allowed = os.getenv('ALLOWED_HOSTS', '')
-ALLOWED_HOSTS = _allowed.split(',') if _allowed else ['*']
+ALLOWED_HOSTS = ['*']
 
 # Nécessaire pour Render (proxy SSL)
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
